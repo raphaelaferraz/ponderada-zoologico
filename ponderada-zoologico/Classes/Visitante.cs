fc1aa79c536bf8fe80a12a2b39284aa4dcf32849
Raphaela Ferraz <raphaelagferraz2@gmail.com>
2024-05-03 09:54:26 -0300

    feat: adiciona classe de Visitante

---
+++ ponderada-zoologico/Classes/Visitante.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ponderada_zoologico.Classes;
+
+public class Visitante
+{
+    // Atributos
+    private string _nome;
+    private int _idade;
+
+    // Construtor
+    public Visitante(string Nome, int Idade)
+    {
+        _nome = Nome;
+        _idade = Idade;
+    }
+
+    // Propriedades
+    public string Nome
+    {
+        get { return _nome; }
+    }
+
+    public int Idade
+    {
+        get { return _idade; }
+    }
+
+}
