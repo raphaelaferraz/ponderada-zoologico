140643e25b1f24e26de78f9b6d6d14551976dcc5
Raphaela Ferraz <raphaelagferraz2@gmail.com>
2024-05-03 09:52:59 -0300

    feat: adiciona testes unitários sobre a classe visitante

---
+++ ponderada-zoologico-testes/TestesVisitante/TestesCriarrVisitante.cs
@@ -0,0 +1,37 @@
+﻿using ponderada_zoologico.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+
+namespace ponderada_zoologico_testes.TestesVisitante;
+
+public class TestesCriarrVisitante
+{
+    private readonly ITestOutputHelper _output;
+
+    public TestesCriarrVisitante(ITestOutputHelper output)
+    {
+        _output = output;
+    }
+
+    [Fact]
+    public void CriarVisitante_DeveConterNomeEIdade()
+    {
+        // Preparação
+        string NomeEsperado = "Raphaela";
+        int IdadeEsperada = 19;
+
+        // Execução
+        Visitante visitante = new Visitante(NomeEsperado, IdadeEsperada);
+
+        // Logging para verificar o que foi criado
+        _output.WriteLine($"Criado visitante: Nome={visitante.Nome}, Idade={visitante.Idade}");
+
+        // Verificação
+        Assert.Equal(NomeEsperado, visitante.Nome);
+        Assert.Equal(IdadeEsperada, visitante.Idade);
+    }
+}
